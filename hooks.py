f02cd07649aeb72fb9c5fcee856cc5ce033bb3a4
Silvio Lugaro <silvio.lugaro@gruppoiren.it>
2024-08-30 09:53:05 +0200

    feat(resource-changelog): recover 2024 history

--- hooks.py
+++ hooks.py
@@ -1,21 +1,88 @@
 import os
+import subprocess
+from collections import defaultdict
+from datetime import datetime
+from typing import Optional
 
 NEWLINE = "\n"
-BLACKLIST = (".pages", "index.md")
 RESOURCES_PATH = "docs/resources"
 
 
-def _index_resources(*args, **kwargs) -> None:
+def get_added_lines(start: Optional[str] = None, end: Optional[str] = None):
+    blacklist = ("about.md", "index.md")
+    # Get the list of files changed in the last commit
+    files_changed = subprocess.check_output(
+        ["git", "show", "-U0", "--pretty=", "--name-only", end],
+        universal_newlines=True,
+    ).splitlines()
+
+    added_lines = defaultdict(list)
+
+    for filename in files_changed:
+        if (
+            filename.endswith(".md")
+            and ("/resources/" in filename)
+            and all(not filename.endswith(name) for name in blacklist)
+        ):
+            # Get the diff for each file
+            diff_output = subprocess.check_output(
+                ["git", "diff", start, end, "--", filename],
+                universal_newlines=True,
+                encoding="utf-8",
+            ).splitlines()
+
+            # Extract added lines
+            for line in diff_output:
+                if (
+                    line.startswith("+")
+                    and not line.startswith("+++")
+                    and line.replace("+", "").replace(" ", "")
+                    and not line.startswith("+#")
+                ):
+                    added_lines[filename].append(line[1:])
+
+    return added_lines
+
+
+def update_resources_changelog(
+    date: Optional[str] = None, start: Optional[str] = None, end: Optional[str] = None
+) -> None:
+    if date is None:
+        date = str(datetime.now().date())
+    if end is None:
+        start, end = "HEAD^", "HEAD"
+
+    added_lines = get_added_lines(start, end)
+    titles = {}
+    for filename in added_lines:
+        try:
+            with open(filename, "r") as f:
+                titles[filename] = f.readlines()[0]
+        except FileNotFoundError:
+            pass
+    added_lines = {k: v for k, v in added_lines.items() if k in titles}
+    changelog = [f"# {date}\n\n"]
+    for filename, lines in sorted(added_lines.items(), key=lambda x: titles[x[0]]):
+        changelog.append(f"#{titles[filename]}\n")
+        for line in sorted(lines):
+            changelog.append(f"{line}\n")
+        changelog.append("\n")
+    with open(f"./docs/resources/changelog/{date}.md", "w", encoding="utf-8") as f:
+        f.writelines(changelog[:-1])
+
+
+def update_resources_index(*args, **kwargs) -> None:
     """Create index for each of the resources sections."""
+    blacklist = (".pages", "index.md", "changelog")
     for section in sorted(os.listdir(RESOURCES_PATH)):
-        if section not in BLACKLIST:
+        if section not in blacklist:
             section_name = (
                 section.replace("-", " ").title() if section.lower() != "aws" else "AWS"
             )
             index = [f"# {section_name}", NEWLINE, NEWLINE]
             path = f"{RESOURCES_PATH}/{section}"
             for file in sorted(os.listdir(path)):
-                if file not in BLACKLIST:
+                if file not in blacklist:
                     with open(f"{path}/{file}", "r", encoding="utf-8") as f:
                         lines = f.readlines()
                         for i, line in enumerate(lines):
@@ -32,4 +99,4 @@ def _index_resources(*args, **kwargs) -> None:
 
 
 if __name__ == "__main__":
-    _index_resources()
+    update_resources_index()
